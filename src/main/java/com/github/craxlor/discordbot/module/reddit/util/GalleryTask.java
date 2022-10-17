c19b6eb5323204f6b5a4a489b087bc50eae80c93
craxlor <arnaud.kalthoff@outlook.de>
2022-10-17 23:11:07 +0200

    refactor: extract post validation to static method for reusability

--- src/main/java/com/github/craxlor/discordbot/module/reddit/util/GalleryTask.java
+++ src/main/java/com/github/craxlor/discordbot/module/reddit/util/GalleryTask.java
@@ -1,9 +1,6 @@
 package com.github.craxlor.discordbot.module.reddit.util;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.FileNameMap;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -12,7 +9,6 @@ import java.util.logging.Logger;
 
 import org.json.simple.parser.ParseException;
 
-import com.github.craxlor.discordbot.reply.EmbedBuilder;
 import com.github.craxlor.jReddit.Listings;
 import com.github.craxlor.jReddit.Reddit;
 import com.github.craxlor.jReddit.RedditPost;
@@ -39,48 +35,27 @@ public class GalleryTask extends TimerTask {
     }
 
     @Override
+    @SuppressWarnings("null")
     public void run() {
         boolean isPostCompatible = false;
-        String post_hint = null, url = null, mimeType = null;
         Random random = new Random();
         RedditPost redditPost = null;
-        FileNameMap fileNameMap = null;
 
         // get some posts (500)
         List<RedditPost> listing = reddit.getListing(subreddit, Listings.HOT, 500);
+        // todo export file validation to make it reusable for other commands
         while (isPostCompatible == false) {
             // select a random post
             redditPost = listing.get(random.nextInt(listing.size()));
             // check if post has been used already
             if (posted.contains(redditPost))
                 continue;
-            // check if post contains usable content
-            post_hint = redditPost.getPost_hint();
-            if (post_hint != null && post_hint.equals("image"))
-                url = redditPost.getUrl_overridden_by_dest();
-            // skip turn if there's no usable content
-            // bot cannot post files bigger than 8 mb
-            if (url == null || new File(url).length() > 8388608)
-                continue;
-            // check mimetype of file
-            fileNameMap = URLConnection.getFileNameMap();
-            mimeType = fileNameMap.getContentTypeFor(url);
-            if (mimeType.indexOf("image") == -1)
-                continue;
-            isPostCompatible = true;
+            isPostCompatible = RedditHelper.canBePosted(redditPost);
         }
-        // TODO save file option -> button or contextmenu
         // send message
-        sendRedditPost(messageChannel, redditPost, url);
+        RedditHelper.sendRedditPost(messageChannel, redditPost);
         posted.add(redditPost);
-        logger.info("posted " + url);
-    }
-
-    // REDDIT-----------------------------------------
-    public static void sendRedditPost(TextChannel channel, RedditPost redditPost, String url) {
-        EmbedBuilder embedBuilder = new EmbedBuilder();
-        embedBuilder.setRedditFormat(channel.getGuild(), redditPost, url);
-        channel.sendMessageEmbeds(embedBuilder.build()).queue();
+        logger.info("posted " + redditPost.getUrl_overridden_by_dest());
     }
 
 }
