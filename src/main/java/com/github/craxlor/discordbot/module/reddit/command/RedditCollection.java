652d0fe2780278debf89ce6e04ae43a898273c81
craxlor <arnaud.kalthoff@outlook.de>
2022-10-17 23:22:47 +0200

    refactor: update RedditCollection
    Signed-off-by: craxlor <arnaud.kalthoff@outlook.de>

--- src/main/java/com/github/craxlor/discordbot/module/reddit/command/RedditCollection.java
+++ src/main/java/com/github/craxlor/discordbot/module/reddit/command/RedditCollection.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 
 import com.github.craxlor.discordbot.command.slash.SlashCommand;
 import com.github.craxlor.discordbot.manager.commandlist.CommandCollection;
+import com.github.craxlor.discordbot.module.reddit.command.slash.Meme;
 import com.github.craxlor.discordbot.module.reddit.command.slash.RedditGallery;
 
 public class RedditCollection extends ArrayList<SlashCommand> implements CommandCollection {
     public RedditCollection() {
         add(new RedditGallery());
+        add(new Meme());
     }
 }
