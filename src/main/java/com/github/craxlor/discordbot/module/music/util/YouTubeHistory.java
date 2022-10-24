9358c1cede0cb97b6856801bcc28912d5162643d
craxlor <arnaud.kalthoff@outlook.de>
2022-10-24 17:28:41 +0200

    feat: store searchTerms to save quotas in youtube api

---
+++ src/main/java/com/github/craxlor/discordbot/module/music/util/YouTubeHistory.java
@@ -0,0 +1,53 @@
+package com.github.craxlor.discordbot.module.music.util;
+
+import javax.annotation.Nullable;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import com.github.craxlor.utilities.JSONFile;
+
+public class YouTubeHistory extends JSONFile {
+
+    private JSONArray history;
+
+    public YouTubeHistory() {
+        super("resources", "youtubehistory");
+        if ((history = (JSONArray) root.get("history")) == null) {
+            history = new JSONArray();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public boolean add(String key, String value) {
+        JSONObject object = new JSONObject();
+        object.put("key", key);
+        object.put("value", value);
+        boolean b = history.add(object);
+        put("history", history, true);
+        return b;
+    }
+
+    public boolean contains(String key) {
+        JSONObject e;
+        for (Object entry : history) {
+            e = (JSONObject) entry;
+            if (e.containsKey(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Nullable
+    public String get(String key) {
+        JSONObject e;
+        for (Object entry : history) {
+            e = (JSONObject) entry;
+            if (e.containsKey(key)) {
+                return (String) e.get("value");
+            }
+        }
+        return null;
+    }
+}
