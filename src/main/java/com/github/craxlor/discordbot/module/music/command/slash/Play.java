85e2ee01c15f3856c36a2840182bbda5aa175b5b
craxlor <arnaud.kalthoff@outlook.de>
2022-10-25 16:43:42 +0200

    refactor: improve input handling to save quotas and provide as much information as possible in any case

--- src/main/java/com/github/craxlor/discordbot/module/music/command/slash/Play.java
+++ src/main/java/com/github/craxlor/discordbot/module/music/command/slash/Play.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import javax.annotation.Nonnull;
 
+import org.json.simple.JSONObject;
+
 import com.github.craxlor.discordbot.manager.GuildManager;
 import com.github.craxlor.discordbot.module.music.command.SCMusic;
 import com.github.craxlor.discordbot.module.music.manager.MusicManager;
 import com.github.craxlor.discordbot.module.music.util.SpotifyHelper;
 import com.github.craxlor.discordbot.module.music.util.YouTubeHelper;
-import com.github.craxlor.discordbot.module.music.util.YouTubeHistory;
+import com.github.craxlor.discordbot.module.music.util.YouTubeStorage;
 import com.github.craxlor.discordbot.reply.Reply;
 import com.github.craxlor.discordbot.reply.Status;
 import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
@@ -37,13 +39,13 @@ public class Play extends SCMusic {
     private static final String NEXT_DESCRIPTION = "play track after the current track";
     private static final String NOW_NAME = "now";
     private static final String NOW_DESCRIPTION = "immediate playback of the provided track";
-    private static final String OPT_NAME = "youtube-url";
-    private static final String OPT_DESCRIPTION = "provide a youtube video or playlist to play";
-    // attributes to avoid scoping problems
+    private static final String OPT_NAME = "input";
+    private static final String OPT_DESCRIPTION = "provide a youtube video/playlist; spotify track url;searchTerm(yt-Search) to play";
+    // attributes to avoid scoping problems in execute()
     private String commandAction = null;
     private AudioTrackInfo trackInfo = null;
     private Status status = Status.ERROR;
-    private String url = null;
+    private String input = null;
 
     public Play() {
         SubcommandData queue = new SubcommandData(QUEUE_NAME, QUEUE_DESCRIPTION);
@@ -71,7 +73,7 @@ public class Play extends SCMusic {
     @Override
     @SuppressWarnings("null")
     public Reply execute(@Nonnull SlashCommandInteractionEvent event) throws Exception {
-        url = event.getOption(OPT_NAME).getAsString();
+        input = event.getOption(OPT_NAME).getAsString();
         Member member = event.getMember();
         String subcommandName = event.getSubcommandName();
         Guild guild = event.getGuild();
@@ -86,33 +88,49 @@ public class Play extends SCMusic {
                         "you have to be in the same AudioChannel");
             }
         }
+
+        YouTubeStorage youTubeStorage = YouTubeStorage.getInstance();
+        JSONObject videoInformation = null;
+        if (input.contains("http") == false && input.contains("www.") == false) {
+            // assume that input is not containing an url but a searchTerm to look up
+            videoInformation = YouTubeHelper.findVideo(input);
+            input = YouTubeHelper.YOUTUBE_VIDEO_PREFIX + (String) videoInformation.get("videoId");
+        }
+        // check if the YT-Video is known in YouTubeStorage
+        else if (input.contains("youtube.com/watch?v=")) {
+            String videoId;
+            // get videoId
+            if (input.contains("&list"))
+                videoId = input.substring(input.lastIndexOf("?v=") + 3, input.indexOf("&list"));
+            else
+                videoId = input.substring(input.lastIndexOf("?v=") + 3, input.length());
+            // retrieve YouTubeStorage entry to provide more information in command-reply
+            if (youTubeStorage.containsVideoId(videoId)) {
+                videoInformation = youTubeStorage.getByVideoId(videoId);
+            }
+        }
         // check if provided url is from spotify
-        if (url.contains("spotify")) {
-            if (url.contains("track") == false)
+        else if (input.contains("open.spotify")) {
+            if (input.contains("track") == false)
                 return new Reply(event.deferReply(), false).onCommand(event, Status.FAIL,
                         "I just support spotify **tracks**");
-            // get track from spotify
-            Track track = SpotifyHelper.getTrack(url);
-            // build searchTerm
-            String artists = "";
-            for (ArtistSimplified artistSimplified : track.getArtists()) {
-                artists += " " + artistSimplified.getName();
-            }
-            String searchTerm = track.getName() + artists;
-            url = getYouTubeUrlBySearchTerm(searchTerm);
-        } else if (url.contains("http") == false) {
-            /**
-             * assume that url is not containing an url but a searchTerm to look
-             * up in youtube
-             */
-            url = getYouTubeUrlBySearchTerm(url);
+            String searchTerm = convertSpotifyUrlToSearchTerm(input);
+            videoInformation = YouTubeHelper.findVideo(searchTerm);
+            input = YouTubeHelper.YOUTUBE_VIDEO_PREFIX + (String) videoInformation.get("videoId");
         }
 
-        if (url.equals("quota"))
+        /**
+         * input should contain a viable url in any case by now
+         * TODO
+         * build a new onMusic reply which can display additional information from
+         * videoInformation JSONObject
+         */
+
+        if (videoInformation != null && videoInformation.get("error") != null)
             return new Reply(event.deferReply(), false).onCommand(event, Status.FAIL,
                     "the daily quota limit has been reached. therfore I cannot do this action");
 
-        GuildManager.getAudioPlayerManager().loadItemOrdered(musicManager, url, new AudioLoadResultHandler() {
+        GuildManager.getAudioPlayerManager().loadItemOrdered(musicManager, input, new AudioLoadResultHandler() {
             @Override
             public void trackLoaded(AudioTrack track) {
                 connectTo(member);
@@ -120,11 +138,10 @@ public class Play extends SCMusic {
                     case QUEUE_NAME -> {
                         musicManager.scheduler.queue(track);
                         // check if the track is played immediately
-                        if (musicManager.player.getPlayingTrack().equals(track)) {
+                        if (musicManager.player.getPlayingTrack().equals(track))
                             commandAction = "playing " + track.getInfo().title;
-                        } else {
+                        else
                             commandAction = track.getInfo().title + " has been added to queue";
-                        }
                     }
                     case NEXT_NAME -> {
                         musicManager.scheduler.addOnTopOfQueue(track, true);
@@ -148,7 +165,7 @@ public class Play extends SCMusic {
                     case NEXT_NAME -> musicManager.scheduler.addOnTopOfQueue(tracks, true);
                     case NOW_NAME -> musicManager.scheduler.addOnTopOfQueue(tracks, false);
                 }
-                commandAction = "added playlist " + "[" + playlist.getName() + "](" + url + ")" + " to queue";
+                commandAction = "added playlist " + "[" + playlist.getName() + "](" + input + ")" + " to queue";
                 status = Status.SUCCESS;
             }
 
@@ -165,7 +182,7 @@ public class Play extends SCMusic {
             }
         }).get();
 
-        return new Reply(event.deferReply(), false).onMusic(event, status, commandAction, trackInfo);
+        return new Reply(event.deferReply(), false).onMusic(event, status, commandAction, trackInfo, videoInformation);
     }
 
     @SuppressWarnings("null")
@@ -175,16 +192,15 @@ public class Play extends SCMusic {
         audioManager.openAudioConnection(myChannel);
     }
 
-    private String getYouTubeUrlBySearchTerm(@Nonnull String searchTerm) {
-        YouTubeHistory youTubeHistory = YouTubeHistory.getInstance();
-        if (youTubeHistory.contains(searchTerm))
-            return youTubeHistory.get(searchTerm);
-        else {
-            String url = YouTubeHelper.getVideoURLBySearchTerm(searchTerm);
-            // save searchTerm to reduce quota usage
-            youTubeHistory.add(searchTerm, url);
-            return url;
+    @SuppressWarnings("null")
+    private String convertSpotifyUrlToSearchTerm(@Nonnull String url) {
+        // get track from spotify
+        Track track = SpotifyHelper.getTrack(url);
+        // build searchTerm
+        String artists = "";
+        for (ArtistSimplified artistSimplified : track.getArtists()) {
+            artists += " " + artistSimplified.getName();
         }
+        return track.getName() + artists;
     }
-
 }
