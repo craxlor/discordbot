2bd14a0bfb1bfb08b1737d689541d5b7284d18ae
craxlor <arnaud.kalthoff@outlook.de>
2022-10-29 16:17:17 +0200

    fix: gradle.properties will be included into jar now

--- src/main/java/com/github/craxlor/discordbot/Properties.java
+++ src/main/java/com/github/craxlor/discordbot/Properties.java
@@ -1,7 +1,7 @@
 package com.github.craxlor.discordbot;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Logger;
 
 import javax.annotation.Nullable;
@@ -14,14 +14,15 @@ public class Properties {
     public static String get(String key) {
         try {
             java.util.Properties properties = new java.util.Properties();
-            properties.load(new FileInputStream("gradle.properties"));
+            InputStream is = Properties.class.getClassLoader().getResourceAsStream("gradle.properties");
+            properties.load(is);
+            is.close();
             return properties.getProperty(key);
         } catch (IOException e) {
             e.printStackTrace();
             logger.warning("""
-                        gradle.properties file cannot be found or the given key cannot be found inside gradle.properties
-                        ---------------------
-
+                    gradle.properties file cannot be found or the given key cannot be found inside gradle.properties
+                    ---------------------
                     """ + e.getMessage());
             return null;
         }
